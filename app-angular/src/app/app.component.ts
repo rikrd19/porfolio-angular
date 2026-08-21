5b6561d67573826452998360b93b537cb00125f5
Ricardo Avila <ricardoavilabazalar@gmail.com>
2026-08-21 22:52:41 +0200

    feat(components): añadir ReplicadorComponent con event binding y one-way binding
    - Uso de (input) para capturar eventos del teclado
    - One-way binding con {{ texto }} para mostrar datos
    - Implementación de actualizarTexto() con Event y cast a HTMLInputElement
    - Template con replicación en tiempo real

--- app-angular/src/app/app.component.ts
+++ app-angular/src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HijoComponent } from "./hijo/hijo.component";
 import { MostrarMensajeComponent } from "./mostrar-mensaje/mostrar-mensaje.component";
+import { ReplicadorComponent } from "./replicador/replicador.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HijoComponent, MostrarMensajeComponent],
+  imports: [RouterOutlet, HijoComponent, MostrarMensajeComponent, ReplicadorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'app-angular';
+  title = 'Property Binding in Angular';
 }
